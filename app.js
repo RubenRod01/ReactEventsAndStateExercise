0ed0ac8a68b71bdc74f334780d9c257403095e60
Ruben Rodriguez <ruben.rdrgz2001@gmail.com>
2021-12-13 09:29:59 -0600

    Completed React Events & State Exercise

---
+++ app.js
@@ -0,0 +1,16 @@
+// 1d
+import Bonus from "./bonus/Bonus";
+
+// 3g
+import Exercise from "./exercise/Exercise";
+function App() {
+  return (
+    <div>
+      {/* 1d */}
+      <Exercise />
+      {/* 3h */}
+      <Bonus />
+    </div>
+  );
+}
+export default App;
